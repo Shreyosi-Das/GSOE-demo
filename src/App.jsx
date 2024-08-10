e3769f36c7eb7442beb325b156924fd008d49d6d
Shreyosi <shreyosidas2003@gmail.com>
2024-08-10 22:10:02 +0530

    shadCN inserted

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <Button className='bg-black text-white' variant='outline' size='sm'>click</Button>
+  )
+}
+
+export default App
